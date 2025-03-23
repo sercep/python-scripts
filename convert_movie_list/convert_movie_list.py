07ea75f63831c1233822b9f885e51fed8157a173
sercep <37669826+sercep@users.noreply.github.com>
2025-03-23 03:25:50 +0300

    add year column to table

--- convert_movie_list/convert_movie_list.py
+++ convert_movie_list/convert_movie_list.py
@@ -6,17 +6,17 @@ def transform_paths(paths):
         match = re.search(r'\\([^\\]+) \((\d{4})\)$', path)
         if match:
             title, year = match.groups()
-            transformed.append(f"[[{title}, {year}]]")
+            transformed.append((f"[[{title}, {year}]]", year))
         else:
             match = re.search(r'\\([^\\]+) \((Season \d+) - (\d{4}(?:-\d{4})?)\)$', path)
             if match:
                 title, season, year = match.groups()
-                transformed.append(f"[[{title}, {season}, {year}]]")
+                transformed.append((f"[[{title}, {season}, {year}]]", year))
             else:
                 match = re.search(r'\\([^\\]+) \((\d{4}-\d{4})\)$', path)
                 if match:
                     title, years = match.groups()
-                    transformed.append(f"[[{title}, {years}]]")
+                    transformed.append((f"[[{title}, {years}]]", years))
     return transformed
 
 def read_paths_from_file(filename):
@@ -25,7 +25,7 @@ def read_paths_from_file(filename):
 
 def write_output_to_file(filename, data):
     with open(filename, 'w', encoding='utf-8') as file:
-        file.write("|     |\n| --- |\n" + '\n'.join(f"| {entry} |" for entry in data))
+        file.write("|     |     |\n| --- | --- |\n" + '\n'.join(f"| {entry} | {year} |" for entry, year in data))
 
 input_filename = "input.txt"
 output_filename = "output.txt"
@@ -34,6 +34,6 @@ paths = read_paths_from_file(input_filename)
 result = transform_paths(paths)
 write_output_to_file(output_filename, result)
 
-print("|     |\n| --- |")
-for r in result:
-    print(f"| {r} |")
+print("|     |     |\n| --- | --- |")
+for entry, year in result:
+    print(f"| {entry} | {year} |")
